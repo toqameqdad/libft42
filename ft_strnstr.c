a116349e48fdd5f5fb6a4b6c972d85b3bae26b91
Toqa Meqdad <toqamok2005@gmail.com>
2025-12-02 19:46:05 +0300

    Fix condition check for empty needle in ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -17,7 +17,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	i;
 	size_t	j;
 
-	if (!needle[i])
+	if (!needle[0])
 		return ((char *)haystack);
 	i = 0;
 	while (i < len && haystack[i])
